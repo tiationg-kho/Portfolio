ca9386600e36c645a9a6b8965e205163f40a34c7
tiationg-kho <tiationgkho@gmail.com>
2023-10-17 21:10:18 -0700

    Update Para Scroll

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,37 @@ function App() {
 
 	return (
 		<div className='app'>
-			<Parallax ref={parallax} pages={3.5}>
+			<Parallax ref={parallax} pages={3.7}>
+				<ParallaxLayer offset={2.2} speed={1.2} style={{ opacity: 0.4 }}>
+					<img
+						src='fav-green.svg'
+						style={{ display: 'block', width: '20%', marginLeft: '5%' }}
+					/>
+					<img
+						src='fav-green.svg'
+						style={{ display: 'block', width: '20%', marginLeft: '85%' }}
+					/>
+				</ParallaxLayer>
+				<ParallaxLayer offset={2.4} speed={-0.4} style={{ opacity: 0.4 }}>
+					<img
+						src='fav-green.svg'
+						style={{ display: 'block', width: '10%', marginLeft: '35%' }}
+					/>
+					<img
+						src='fav-green.svg'
+						style={{ display: 'block', width: '20%', marginLeft: '65%' }}
+					/>
+				</ParallaxLayer>
+				<ParallaxLayer offset={3.2} speed={0.8} style={{ opacity: 0.4 }}>
+					<img
+						src='fav-green.svg'
+						style={{ display: 'block', width: '20%', marginLeft: '15%' }}
+					/>
+					<img
+						src='fav-green.svg'
+						style={{ display: 'block', width: '10%', marginLeft: '95%' }}
+					/>
+				</ParallaxLayer>
 				<ParallaxLayer offset={0} speed={-0.1}>
 					<HeroSection scrollToHandle={scrollToHandle} />
 				</ParallaxLayer>
@@ -22,8 +52,8 @@ function App() {
 					<AboutMe />
 				</ParallaxLayer>
 				<ParallaxLayer
-					offset={2.9}
-					speed={0.15}
+					offset={3}
+					speed={0.2}
 					onClick={() => parallax.current.scrollTo(0)}
 				>
 					<Footer />
